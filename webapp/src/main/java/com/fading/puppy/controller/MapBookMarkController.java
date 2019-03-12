a8096bde025f20f4fd49ab0b643cb64c32018ad0
huangyong <yong.huang@southgis.com>
2019-03-12 17:14:01 +0800

    swagger

--- webapp/src/main/java/com/fading/puppy/controller/MapBookMarkController.java
+++ webapp/src/main/java/com/fading/puppy/controller/MapBookMarkController.java
@@ -4,6 +4,9 @@ import com.fading.puppy.entity.MapBookMark;
 import com.fading.puppy.service.IMapBookMarkService;
 import com.fading.puppy.tools.AccessTokenOrJsapiTicketUtil;
 import com.fading.puppy.tools.ExportTextUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +22,7 @@ import java.util.Map;
 import org.json.JSONObject;
 
 @Controller
+@Api(tags = "图签接口")
 @RequestMapping("/mapbookmark")
 public class MapBookMarkController {
     @Inject
@@ -40,6 +44,8 @@ public class MapBookMarkController {
     /**
      * 保存或修改地图书签/标注
      */
+    @ApiOperation(value = "保存图签", notes = "")
+    @ApiImplicitParam(name = "bookMarkStr", value = "图签信息", required = true, dataType = "String")
     @RequestMapping(value = "/saveUpdateMapBookMark",method = RequestMethod.POST)
     @ResponseBody
     public Map<String,String> saveUpdateMapBookMark(String bookMarkStr){
